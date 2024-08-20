e3d280b5248ba3c718055ebaa412876991550671
virginiachambi <163939844+virginiachambi@users.noreply.github.com>
2024-08-19 20:31:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 def crear_app():
     app = Flask(__name__)
 
-    # Crear el directorio de gráficos si no existe
     os.makedirs('static/plots', exist_ok=True)
 
     @app.route("/", methods=["GET", "POST"])
@@ -14,15 +13,12 @@ def crear_app():
         plot_file = None
         if request.method == "POST":
             try:
-                # Obtener los coeficientes de la ecuación lineal de la forma y = mx + b
                 m = float(request.form.get("m"))
                 b = float(request.form.get("b"))
 
-                # Generar puntos para la gráfica
                 x = np.linspace(-10, 10, 400)
                 y = m * x + b
 
-                # Crear la gráfica
                 plt.figure()
                 plt.plot(x, y, label=f'y = {m}x + {b}', color='blue')
                 plt.axhline(0, color='black', linewidth=0.5, ls='--')
@@ -33,7 +29,6 @@ def crear_app():
                 plt.grid()
                 plt.legend()
 
-                # Guardar la imagen
                 plot_file = 'static/plots/plot.png'
                 plt.savefig(plot_file)
                 plt.close()
@@ -49,6 +44,7 @@ def crear_app():
 
     return app
 
+app = crear_app()
+
 if __name__ == "__main__":
-    app = crear_app()  # Crear la instancia de la aplicación
-    app.run(debug=True)  # Ejecutar la aplicación
+    app.run(debug=True)
